61474dd5dece0d4d9e9ce2953550325b1f4f9997
ronaldoaf <ronaldo.a.f@gmail.com>
2018-11-05 10:56:18 -0200

    Update get_pages.user.js

--- get_pages.user.js
+++ get_pages.user.js
@@ -11,13 +11,13 @@
 
 setInterval(function(){
     var obj={};
-    $.get('https://aposte.me/totalcorner_stats_half/select_page.php', function(data){
+    $.get('https://bot-ao.com/half/select_page.php', function(data){
         $.get('/match/schedule/'+data, function(res){
             obj.data=Number(data);
             obj.n_pages=$("li a[rel='last']:eq(0)").size()==0 ? 1 : Number($("li a[rel='last']:eq(0)").attr('href').split(':')[1]);
             $.ajax({
                 type: 'POST',
-                url: 'https://aposte.me/totalcorner_stats_half/insert_pages.php',
+                url: 'https://bot-ao.com/half/insert_pages.php',
                 data: JSON.stringify (obj),
                 success: function(res_insert) {
                     console.log(res_insert)
